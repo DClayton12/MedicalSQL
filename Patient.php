1ff60c7b539f2be03fb1e357c8b813b35532a449
Darnel Clayton <darnelclayton@gmail.com>
2015-03-02 22:13:39 -0500

    Initial commit

---
+++ Patient.php
@@ -0,0 +1,70 @@
+<?php
+//Darnel Clayton , Robert Brancale
+ini_set('display_errors', 'On'); //Error reporting
+$mysqli = new mysqli('oniddb.cws.oregonstate.edu','claytond-db','0scRzn1A9MkIFkzK','claytond-db'); //connect to DB
+if ($mysqli->connect_error) {
+    die('Cannot connect to SQL Database. (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+//  get PATIENT variables from POST request
+$dob= $_POST['dob'];
+$fname= $_POST['fname'];
+$lname= $_POST['lname'];
+$phone= $_POST['phone'];
+$house= $_POST['house'];
+$street_name= $_POST['street_name'];
+$town= $_POST['town'];
+
+if(!empty($dob) && !empty($fname) && !empty($lname) && !empty($phone) && !empty($house) && !empty($street_name) && !empty($town)){
+	//Only if user inputs all specified data. 
+	if(!($stmt = $mysqli->prepare("INSERT INTO Patient(dob, fname, lname, phone, house, street_name, town) VALUES (?,?,?,?,?,?,?)"))){
+		//Prepare INSERT query
+				echo "Prepare failed: "  . $stmt->errno . " " . $stmt->error;
+	}
+	if(!($stmt->bind_param("dssddss",$_POST['dob'],$_POST['fname'],$_POST['lname'],$_POST['phone'],$_POST['house'],$_POST['street_name'],$_POST['town']))){
+			//Referenced: http://php.net/manual/en/mysqli-stmt.bind-param.php	
+			echo "Bind failed: "  . $stmt->errno . " " . $stmt->error;
+			}
+	if(!$stmt->execute()){
+	// Execute query
+	echo "Execute failed: "  . $stmt->errno . " " . $stmt->error;
+	}
+	$stmt->close(); // Redirect back to main page now that insert query is done
+	$path = explode('/', $_SERVER['PHP_SELF'], - 1);
+	$path = implode('/', $path);
+	$redirect = "http://" . $_SERVER['HTTP_HOST'] . $path;
+	header("Location: {$redirect}/index.php");
+
+	if(empty($dob)) { // One or more inputs are empty. These may not be NULL.
+		echo 'Birthdate is a required field. ';
+	}
+	if(empty($fname)) {
+		echo 'First name is a required field. ';
+	}
+	if(empty($lname)) {
+		echo 'Last name is a required field. ';
+	}
+	if(empty($phone)) {
+		echo 'Phone is a required field. ';
+	}
+	if(empty($house)) {
+		echo 'House number is a required field. ';
+	}
+	if(empty($street_name)) {
+		echo 'Street name is a required field. ';
+	}
+	if(empty($town)) {
+		echo 'Town is a required field. ';
+	}
+	}
+?>
+
+<!DOCTYPE html> 
+<html>
+  <head>
+    <title>Clayton, Brancale CS340 Final</title>
+  </head>
+  <body>
+  <p>
+  <a href="index.php">Click here to return to the main page.</a>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+//Darnel Clayton , Robert Brancale
+ini_set('display_errors', 'On'); //Error reporting
+$mysqli = new mysqli('oniddb.cws.oregonstate.edu','claytond-db','0scRzn1A9MkIFkzK','claytond-db'); //connect to DB
+if ($mysqli->connect_error) {
+    die('Cannot connect to SQL Database. (' . $mysqli->connect_errno . ') ' . $mysqli->connect_error);
+}
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <title>Clayton Brancale CS340 Final</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.2/sandstone/bootstrap.min.css" rel="stylesheet"/>
+    <h2>CS340 Final</h2>
+      <h4>by Robert Brancale and Darnel Clayton</h4>
+    <style>
+    	body {
+    		margin-top: 80px;
+    	}
+    </style>
+  </head>
+  <body>
+    <br>
+    <br>
+    <br>
+  <div>
+  	<form action="Patient.php" method="post" id="Patient">
+				<div class="ptInfo">
+          <label class="col-sm-2 control-label">Patient Information:</label>
+          <div class="col-sm-6">
+            <input class="form-control" id="fname" name="fname" placeholder="First name.." type="text">
+            <input class="form-control" id="lname" name="lname" placeholder="Last name.." type="text">
+            <input class="form-control" id="dob" name="dob" placeholder="Birthdate.." type="date">
+            <input class="form-control" id="phone" name="phone" placeholder="Phone.." type="number">
+            <input class="form-control" id="house" name="house" placeholder="Street Address.." type="number">
+            <input class="form-control" id="street_name" name="street_name" placeholder="Street.." type="text">
+            <input class="form-control" id="town" name="town" placeholder="Town.." type="text">
+            <input class="form-control" id="state" name="state" placeholder="State.." type="state">
+          </div>
+        </div>
+        <div class="form-group">
+          <div class="col-sm-2 col-sm-offset-8">
+           <button type="submit" class="btn btn-success btn-block">Submit</button>
+          </div>
+        </div>
+   	</form>
+   	</div>
+  </body>
+</html>
